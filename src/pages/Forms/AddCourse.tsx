cf4ffc41a0b275eadfdc549d0d8f7550b78829de
itsbasilsaman <itsbasilsaman@gmail.com>
2025-08-30 12:53:05 +0530

    Build Error Solved

--- src/pages/Forms/AddCourse.tsx
+++ src/pages/Forms/AddCourse.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { Transition } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
 
